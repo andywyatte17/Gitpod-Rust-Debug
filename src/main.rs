2256830b34af61518f04056a6aa07ffffb377cc0
andywyatte17 <andywyatte17@gmail.com>
2020-11-21 09:21:01 +0000

    Started guessing-game example.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::io;
+
 fn main() {
     let x = 5;
     println!("Hello, world!");
     for i in 0..5 {
         println!("{}", i);
     }
+
+    loop {
+      let mut guess = String::new();
+      io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+      println!("Here's your guess - {}", guess);
+    }
+
     println!("XX");
 }
