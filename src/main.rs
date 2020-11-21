0097178c6a44aca46918bb324c8c6714614cf3a1
andywyatte17 <andywyatte17@gmail.com>
2020-11-21 09:57:07 +0000

    Moved guessing game to use buffer instead of stdin.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use std::io;
+use std::io::BufRead;
 
 fn main() {
-    let x = 5;
-    println!("Hello, world!");
-    for i in 0..5 {
-        println!("{}", i);
-    }
+    let cursor = io::Cursor::new(b"1\n2\n123\n");
+    let mut line_reader = io::BufReader::new(cursor);
+
+    println!("YY");
 
     loop {
       let mut guess = String::new();
-      io::stdin().read_line(&mut guess)
+      print!("Please type in your guess: ");
+      //io::stdin().read_line(&mut guess)
+      //  .expect("Failed to read line");
+      line_reader.read_line(&mut guess)
         .expect("Failed to read line");
       println!("Here's your guess - {}", guess);
+      if guess=="123" || guess=="123\n" {
+          break;
+      }
     }
 
     println!("XX");
